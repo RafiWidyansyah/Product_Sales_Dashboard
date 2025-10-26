5208bf511fb2d3139f99c7ff346d95f8a15eaf9d
RafiWidyansyah <61896896+RafiWidyansyah@users.noreply.github.com>
2025-10-26 18:53:03 +0700

    Update product_sales_dashboard.py

--- product_sales_dashboard.py
+++ product_sales_dashboard.py
@@ -7,15 +7,24 @@ import streamlit as st
 # Load Dataset
 data = pd.read_csv('product_sales_clean.csv')
 
+# Create Filter By "Week"
+min_value, max_value = st.slider("Select Week :",
+                                 min_value=int(data['week'].min()),
+                                 max_value=int(data['week'].max()),
+                                 value=(min_value, max_value))
+
+
+main_data = data[(data['week'] >= min_value) & (data['week'] <= max_value)]
+
 # Number of Customers per Methods
-num_cust_by_sales_method = data['sales_method'].value_counts()
+num_cust_by_sales_method = main_data['sales_method'].value_counts()
 
 # Revenue Over Time By Sales Method
-revenue_over_time = data.groupby(['week', 'sales_method'])['revenue'].sum()
+revenue_over_time = main_data.groupby(['week', 'sales_method'])['revenue'].sum()
 
 # Business Metrics
 # Average Revenue per Customer by Sales Method Over Time
-avg_revenue_cust_time = data.groupby(['week', 'sales_method']).agg({'revenue':'sum', 
+avg_revenue_cust_time = main_data.groupby(['week', 'sales_method']).agg({'revenue':'sum', 
                                                                                 'customer_id' :'count'}).reset_index()
 avg_revenue_cust_time['avg_revenue_by_customer'] = avg_revenue_cust_time['revenue']/avg_revenue_cust_time['customer_id']
 pivot = avg_revenue_cust_time.pivot_table(index='week', columns='sales_method', values='avg_revenue_by_customer')
@@ -25,11 +34,6 @@ pivot = avg_revenue_cust_time.pivot_table(index='week', columns='sales_method', 
 st.set_page_config(page_title="Pens & Printers New Product Sales Dashboard",
                    page_icon="bar_chart:",
                    layout="wide")
-
-## Week Selector
-week = data['week'].unique()
-with st.container(border=True):
-    users = st.multiselect("Week", week, default=week)
 
 ## Main Page
 st.title("New Product Sales Dashboard")
